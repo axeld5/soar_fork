b9b453dd4e9fb4d0992d247b451f2ba380332900
Julien Pourcel <j.pourcel31830@gmail.com>
2025-06-22 19:44:20 +0200

    initial commit (full code)

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# SOAR
-## Coming Soon
+# Self-Improving Language Models for Evolutionary Program Synthesis: A Case Study on ARC-AGI
+
+<p align="center">
+          🤗 <a href="https://huggingface.co/collections/julien31/soar-arc-6856d27681fce01d9af4c4a3">Hugging Face (data and model)</a>&nbsp&nbsp | &nbsp&nbsp 📑 <a href="todo">Paper</a> &nbsp&nbsp | &nbsp&nbsp 📑 <a href="https://julienp.netlify.app/posts/soar/">Blog</a>
+</p>
+
+Large Language Models (LLMs) have become incredibly powerful, but they often hit a wall when faced with truly complex reasoning tasks that require discovering a solution from scratch. Simply throwing more computing power or using a bigger model often yields diminishing returns. But what if a model could learn from its own experience, getting smarter with every attempt?
+
+We introduce a framework called **SOAR (Self-improving Operators for Automated program Refinements)** that does just that. By creating a "virtuous cycle" of evolutionary search and learning, SOAR enables AI models to bootstrap their own capabilities and solve problems previously beyond their reach. we tested SOAR on the Abstraction and Reasoning Corpus (ARC-AGI-1), a notoriously difficult benchmark designed to challenge an AI's core reasoning abilities. We show that using SOAR with only open weight LLM, we can significantly outperforming much larger closed source LLMs.
+
+## SOAR framework
+![soar](docs/image/soar_fig.png)
+
+1.  **Evolutionary Search (Sample & Refine):** SOAR uses an LLM to generate an initial pool of thousands of candidate programs (the "sampling" step). It then tests these programs and uses the LLM again to intelligently modify or "refine" the most promising ones based on their performance.
+
+2.  **Learning from Hindsight:** SOAR takes all the programs generated during the search phase—including both successes and failures—and uses them as training data. The key insight is that any failed program is simply a *correct* program for a *different* task. By "relabeling" these failed attempts as correct solutions for the synthetic tasks they inadvertently solve, SOAR creates a diverse dataset to learn from.
+
+This process creates a powerful feedback loop: the fine-tuned model becomes better at sampling and refining, which leads to a more effective search in the next iteration, which in turn generates even better training data. And unlike previous approaches that rely on human-engineered domain-specific languages or human-generated solutions, SOAR learns to synthesize programs in Python solely from its own synthesis attempts, encompassing both successes and failures.
+
+
+## info install
+
+### conda inference env
+```
+pip install --upgrade pip
+
+git clone https://github.com/flowersteam/SOAR
+cd SOAR
+conda create --name sglang47 \
+    python=3.11 \
+    -y
+conda activate sglang47
+
+pip install "sglang[all]>=0.4.7"
+
+pip install -e .
+pip install -r requirements
+
+```
+
+### conda train env
+```
+conda create --name unsloth_env \
+    python=3.11 \
+    pytorch-cuda=12.1 \
+    pytorch cudatoolkit xformers -c pytorch -c nvidia -c xformers \
+    -y
+conda activate unsloth_env
+
+pip install unsloth
+cd SOAR
+pip install -e .
+pip install -r requirements.txt
+```
+
+## Run SOAR
+To run SOAR, please refer to execution instructions located in the experience folder. 
---
+++ experience/qwen.sh
@@ -0,0 +1,115 @@
+# download models (if needed)
+huggingface-cli download Qwen/Qwen2.5-Coder-3B-Instruct --local-dir Qwen2.5-Coder-3B-Instruct --local-dir-use-symlinks True --cache-dir ./hf_cache
+huggingface-cli download nomic-ai/CodeRankEmbed --local-dir CodeRankEmbed --local-dir-use-symlinks True --cache-dir ./hf_cache
+
+
+# init variables
+export work_path="/home/flowers/work"
+export soar_path="$work_path/SOAR"
+export model_id_base="Qwen2.5-Coder-3B-Instruct" # "Qwen3-1.7B" "Qwen2.5-Coder-3B-Instruct" #
+export path_base_model="$work_path/hf/$model_id_base"
+export path_embed_model="$work_path/hf/CodeRankEmbed"
+export split="train" # "train" "val"
+export n_sample_task=4
+export model_id="${model_id_base}_n${n_sample_task}"
+# generation (0,1,2,3,...) 
+export gen="0"
+
+
+# if gen is 0, we use the base model
+if [ "$gen" -eq 0 ]; then
+    export path_model="$work_path/hf/$model_id_base"
+    
+else
+    export path_model="$work_path/hf/trained/$model_id_base/gen-$gen/$model_id"
+fi
+
+
+# init inference environment 
+conda deactivate
+conda activate sglang47
+
+# Sample phase (default fewshot example from train_solution.pkl but should be from train archive of previous generation)
+python $soar_path/soar/inference/sample_phase.py \
+--base_path $soar_path/ \
+--path_model $path_model/ \
+--path_save_res $soar_path/save_results/$model_id_base/gen-$gen/solution \
+-k 3000 \
+--model_len 30000 \
+--gpu_mem 0.89 \
+--n_gpu 1 \
+--split $split \
+--bs_inference 1 \
+--seed 0 \
+--max_tokens 4096 \
+--path_fewshot $soar_path/soar/inference/train_solutions.pkl
+
+# Process results sample phase
+python $soar_path/soar/post_process/merge_filter.py \
+--base_path $soar_path/ \
+--path_folder $soar_path/save_results/$model_id_base/gen-$gen/ \
+--max_data 3000 \
+--split $split 
+
+# Refinement phase
+for seed in {0..1}; do
+    python $soar_path/soar/repair/rex_inference.py \
+    --base_path $soar_path/ \
+    --path_model $path_model/ \
+    --path_archive $soar_path/save_results/$model_id_base/gen-$gen/solution/train_sol.pkl \
+    --model_len 30000 \
+    --total_budget 600 \
+    --seed $seed \
+    --split $split \
+    --max_tokens 2048 \
+    --n_completion 1 \
+    --gpu_mem 0.87
+done
+
+# Process results sample&refinement phase
+python $soar_path/soar/post_process/merge_filter.py \
+--base_path $soar_path/ \
+--path_folder $soar_path/save_results/$model_id_base/gen-$gen/ \
+--max_data 3000 \
+--split $split \
+--use_repair_data
+
+# optional merge results and deduplication in one results
+python $soar_path/soar/post_process/dedup.py \
+--base_path $soar_path/ \
+--path_embed_model $path_embed_model/ \
+--path_save $soar_path/save_results/$model_id_base/gen-$gen/solution/train_sol_repair.pkl \
+--path_0 $soar_path/save_results/$model_id_base/gen-$gen/solution/train_sol_repair.pkl 
+# --path_1 "path_to_your_refinement_data" \
+# can put up to path_20
+
+# sample training data for sample training (use train_sol_repair.pkl in path_folder_solution as data)
+python $soar_path/soar/post_process/process_sample_for_training.py \
+--path_model $path_model/ \
+--path_embed_model $path_embed_model/ \
+--split $split \
+--N_sample_task $n_sample_task \
+--path_folder_solution $soar_path/save_results/$model_id_base/gen-$gen/solution/ \
+--sampling_her greedy_div 
+
+# sample training data for sample refinement (use every data starting with $split in path_folder_refinement as data)
+python $soar_path/soar/post_process/process_repair_for_training.py \
+--base_path $soar_path/ \
+--path_embed_model $path_embed_model/ \
+--split $split \
+--N_sample_task $n_sample_task \
+--path_folder_refinement $soar_path/save_results/$model_id_base/gen-$gen/refinement/
+
+
+# Train model (add --load_in_4bit for Q-LoRA)
+conda deactivate
+conda activate unsloth_env
+python $soar_path/soar/training/train_unsloth.py \
+--base_path $soar_path/ \
+--model_len 30000 \
+--path_base_model $path_base_model \
+--path_archive $soar_path/save_results/$model_id_base/gen-$gen/solution/data4train-train-n$n_sample_task-greedy_div.pkl \
+--path_archive_repair $soar_path/save_results/$model_id_base/gen-$gen/refinement/data4train-train-n$n_sample_task.pkl \
+--path_save_model $work_path/hf/trained/$model_id_base/gen-$((gen + 1))/$model_id 
+# Loop
+
